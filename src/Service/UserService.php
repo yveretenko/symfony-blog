4cc971d5ad7573202e45e7a942483322c5bac75d
Yury Veretenko <yura11v@gmail.com>
2025-10-26 22:50:59 +0200

    Create UserService with createAndFlush()

---
+++ src/Service/UserService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+readonly class UserService
+{
+    public function __construct(private EntityManagerInterface $em) {}
+
+    public function createAndFlush(string $username, string $firstName, string $lastName): User
+    {
+        $user = (new User())
+            ->setUsername($username)
+            ->setFirstName($firstName)
+            ->setLastName($lastName);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return $user;
+    }
+}
