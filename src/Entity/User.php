a03f2ff275bc5d4b0bac912fef978450ff17dcae
Yury Veretenko <yura11v@gmail.com>
2025-10-27 14:49:25 +0200

    Make username unique in User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,7 @@ class User
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
