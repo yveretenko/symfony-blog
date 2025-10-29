551dea477ca88033f60dc20c223742c160a2dbbf
Yury Veretenko <yura11v@gmail.com>
2025-10-29 16:22:39 +0200

    Add console command to create a new admin user

---
+++ src/Command/CreateAdminUserCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\UserService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:create-admin-user',
+    description: 'Creates a new admin user.',
+)]
+class CreateAdminUserCommand extends Command
+{
+    public function __construct(private readonly UserService $userService)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('username', InputArgument::REQUIRED, 'Username of the admin')
+            ->addArgument('firstName', InputArgument::REQUIRED, 'First name')
+            ->addArgument('lastName', InputArgument::REQUIRED, 'Last name');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = $this->userService->createAndFlush(
+            $input->getArgument('username'),
+            $input->getArgument('firstName'),
+            $input->getArgument('lastName')
+        );
+
+        $output->writeln("<info>Admin user created with ID {$user->getId()}</info>");
+
+        return Command::SUCCESS;
+    }
+}
